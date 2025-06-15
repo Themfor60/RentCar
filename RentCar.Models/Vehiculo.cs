79a718c6349ce1231bf90ca96ac4302fc312d398
Uriel Aquino <143182625+Themfor60@users.noreply.github.com>
2025-06-15 18:10:45 -0300

    ------

--- RentCar.Models/Vehiculo.cs
+++ RentCar.Models/Vehiculo.cs
@@ -15,16 +15,13 @@ namespace RentCar.Models
         public int CapacidadPersonas { get; set; }
         public string? CapacidadMaletero { get; set; }
         public decimal Precio { get; set; } 
-
-<<<<<<< HEAD
-
         public byte[]? Foto { get; set; }
 
-=======
-        public byte[]? Foto { get; set; }
+
+  
 
         
->>>>>>> 702075e0e8f84faae4213dd633f4c3bbd795753a
+
 
     }
 }
